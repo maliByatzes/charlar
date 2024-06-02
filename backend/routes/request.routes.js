62d72ff81e1c388bcde9103d2fac0dbb1179deac
maliByatzes <malib2027@gmail.com>
2024-06-02 21:08:12 +0200

    add request routes

--- backend/routes/request.routes.js
+++ backend/routes/request.routes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { sendRequestHandler } from "../controllers/request.controller.js";
+import {
+  cancelRequestHandler,
+  getReceivedRequests,
+  getSentRequests,
+  sendRequestHandler,
+  updateRequestHandler
+} from "../controllers/request.controller.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.post('/send/:receiverId', protectRoute, sendRequestHandler);
+router.get('/sent', protectRoute, getSentRequests);
+router.get('/received', protectRoute, getReceivedRequests);
+router.patch('/update/:id', protectRoute, updateRequestHandler);
+router.patch('/cancel/:id', protectRoute, cancelRequestHandler);
 
 export default router;
